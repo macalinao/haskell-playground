6056cfffc8b77f614c9964b8a8279f47f43e538c
Ian Macalinao <me@ian.pw>
2017-04-22 22:06:25 -0500

    initial commit with factor anamorphism

---
+++ app/FactorAnamorphism.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module FactorAnamorphism where
+
+import Data.Functor.Classes
+import Data.Functor.Foldable
+import Data.List
+
+nextPrimeFactor :: Integer -> Maybe Integer
+nextPrimeFactor n = find (\x -> n `mod` x /= 0) [2..(floor $ sqrt $ fromIntegral n)]
+
+data ExprF r = FactorF Integer | MultF r r deriving (Show, Functor)
+type Expr = Fix ExprF
+
+factor :: Integer -> Expr
+factor = ana coAlg where
+  coAlg fac = case (nextPrimeFactor fac) of
+    Just prime -> MultF prime (fac `div` prime)
+    Nothing    -> FactorF fac
+
+main :: IO ()
+main = print $ factor 10
